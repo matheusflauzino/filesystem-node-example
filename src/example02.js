9e9ff5db18aa3595be866d3c6a77a59517539a1b
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2023-10-06 22:54:40 -0300

    refactor: melhoria e exemplo de zip de arquivo grande

--- src/example02.js
+++ src/example02.js
@@ -4,9 +4,9 @@ import { Transform, Writable } from 'node:stream';
 
 let count = 0;
 
-const readableStream = fs.createReadStream('./organizations-1000000.csv', 'utf-8');
+const readableStream = fs.createReadStream('./tmp/organizations-1000000.csv', 'utf-8');
 const gzipStream = zlib.createGzip();
-const writableStream = fs.createWriteStream('./arquivo_compactado.csv.gz')
+const writableStream = fs.createWriteStream(`./tmp/compress_${Date.now()}.csv.gz`)
 
 const execute = (async function () {
     console.log('Begin');
